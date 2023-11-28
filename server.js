bc3eaba0a182c63f0449d61f299495dc3c01b43a
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-28 13:38:38 +1030

    sample routes added WIP

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 //import express.js server
 const express = require('express');
 
-//import routes
-const api = require('./routes/index.js');
-
 //Import and require mysql2
 const mysql = require('mysql2');
 
@@ -30,10 +27,84 @@ const db = mysql.createConnection(
     console.log(`Connected to the movie_database.`)
 );
 
-//Query database
-db.query('SELECT * FROM movie', function (err, results)
-{
-    console.log(results);
+// GET request to the /api/movies route renders a list of all movies.
+app.get('/api/movies', (req, res) => {
+    const sql = 'SELECT id, movie_name AS title FROM movies';
+    
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error:err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: results 
+        });
+    });
+});
+
+// GET request to the /api/movie-reviews route renders a list of all reviews with movie data.
+app.get('api/movie-reviews', (req, res) => {
+    const sql = 'SELECT movies.movie_name AS title, reviews.review AS review FROM reviews JOIN movies ON reviews.movie_id=movies.id;'
+
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error:err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: results
+        });
+    });
+});
+
+//POST request to the /api/add-movie route adds a movie when tested using Insomnia.
+app.post('api/add-movie', (req, res) => {
+    const sql = `INSERT INTO movies (id, movie_name) VALUES (++, ?);`
+
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error:err.message });
+            return;
+        }
+        res.json({
+            message: 'Movie successfully added',
+            data: results
+        });
+    });
+});
+
+//PUT request to the /api/review/:id route updates a movie review.
+app.put('/api/review/:id', (req, res) => {
+    const sql = `UPDATE reviews.review SET review = "?" WHERE id = ?;`
+
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error:err.message });
+            return;
+        }
+        res.json({
+            message: "Review sucessfully updated",
+            data: results
+        });
+    });
+});
+
+//DELETE request to /api/movie/:id route deletes a movie when tested using Insomnia.
+app.delete('api/movie/:id', (req, res) => {
+    const sql = 'DELETE FROM movies WHERE id = ?'
+
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error:err.message });
+            return;
+        }
+        res.json({
+            message: "Movie successfully deleted from database",
+            data: results
+        });
+    });
 });
 
 //Default response for any other request (not found)
